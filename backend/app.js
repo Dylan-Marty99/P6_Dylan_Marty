622fdb22ba9c12984d39059777e237fffa911848
Dylan-Marty99 <dylan.marty99@gmail.com>
2021-10-25 11:46:11 +0200

    Création d'un compte et connection + token (bug)

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,30 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/user");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://DyMty:mongodbusermarty@cluster0.bhvbh.mongodb.net/test?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://DyMty:mongodbusermarty@cluster0.bhvbh.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
   console.log("Requête reçue !");
@@ -28,5 +45,6 @@ app.use((req, res, next) => {
   console.log("Réponse envoyée avec succès !");
 });
 
-module.exports = app;
+app.use("/api/auth", userRoutes);
 
+module.exports = app;

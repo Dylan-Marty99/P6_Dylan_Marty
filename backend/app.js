cfa6bc26f238603347b697d4f30edd464a783251
Dylan-Marty99 <dylan.marty99@gmail.com>
2021-10-25 13:55:57 +0200

    Création des routes en suivant l'exemple du cours
    Modifier les routes correctement

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
+const stuffRoutes = require("./routes/stuff");
 const userRoutes = require("./routes/user");
+
+const path = require("path");
 
 const app = express();
 
@@ -26,25 +30,10 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
-
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/stuff", stuffRoutes);
 app.use("/api/auth", userRoutes);
 
 module.exports = app;
